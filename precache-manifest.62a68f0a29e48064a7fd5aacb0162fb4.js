c373715cbaad9b4cd4642c3326c6a1606ce91226
Nguyen Sy Manh Cuong <cuongnsm@gmail.com>
2021-05-04 11:11:38 +0700

    deploy

---
+++ precache-manifest.62a68f0a29e48064a7fd5aacb0162fb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b04c1ed36da68d40499",
+    "url": "/css/app.601d2ada.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f78a95d444d6e48ac3dcdd0265529be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abce201a8ab32445d218",
+    "url": "/js/about.77d9d8c6.js"
+  },
+  {
+    "revision": "0b04c1ed36da68d40499",
+    "url": "/js/app.70a7a422.js"
+  },
+  {
+    "revision": "6f41ff294961e91e002d",
+    "url": "/js/chunk-vendors.86713306.js"
+  },
+  {
+    "revision": "f449d5e0616c17b5c11778a11b509cf1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
